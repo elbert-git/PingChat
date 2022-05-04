ea6a2cd9d2d4ad4fbc26fa09ae77f934549c69d7
Elbert Nathanael <elbertwithane@gmail.com>
2022-05-04 20:06:40 +0800

    filePathIssue

--- src/assets/earth/Earth.js
+++ src/assets/earth/Earth.js
@@ -8,7 +8,7 @@ import { useGLTF } from '@react-three/drei'
 
 export default function Model({ ...props }) {
   const group = useRef()
-  const { nodes, materials } = useGLTF(earthModel);
+  const { nodes, materials } = useGLTF(earthModel)
   return (
     <group ref={group} {...props} dispose={null}>
       <mesh geometry={nodes.BakingSphere.geometry} material={materials.Material} scale={0.86} />
